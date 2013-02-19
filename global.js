0d3d19b5e3cf70a79ecc58223d9c83b1ad8e07e5
Ross Singer <rossfsinger@gmail.com>
2013-02-18 20:28:38 -0500

    Path for var

--- global.js
+++ global.js
@@ -3,7 +3,7 @@ var isbn = require('isbn').ISBN;
 var fs = require('fs');
 
 // Load full global groups
-var groupdata = JSON.parse(fs.readFileSync('./var/isbn-groups.json','utf8'));
+var groupdata = JSON.parse(fs.readFileSync(path.resolve(__dirname, '/var/isbn-groups.json'),'utf8'));
 isbn.GROUPS = groupdata.GROUPS;
 isbn.GROUPS_VERSION = groupdata.GROUPS_VERSION;
 
